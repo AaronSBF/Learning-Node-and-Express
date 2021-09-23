c0fdbb86c5333f5a6fa9c925fd7631e98d2612be
Aaron <aaronkelly883@gmail.com>
2021-09-23 20:45:28 +0100

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+//Use body-parser to Parse POST Requests
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 console.log('Hello world');
 
