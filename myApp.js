c9f8b37b5d560852687afbeb5f06cfbee4b8043b
Aaron <aaronkelly883@gmail.com>
2021-09-23 15:57:03 +0100

    added chained middleware for the get route method to create time server

--- myApp.js
+++ myApp.js
@@ -9,10 +9,13 @@ app.use("/", function(req, res, next){
     })
 
 
-    app.get("/now", function(req, res, next){ 
+    app.get("/now", 
+    function(req, res, next){ 
         req.time = new Date().toString();
           next();
-        }, function(req, res){ 
+        }, 
+        
+        function(req, res){ 
         res.send({time: req.time})})
         
 
